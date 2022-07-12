ac241e378b5d17b864c14111935fa64c187f4da5
John Heisler <108553174+Snowstuff123@users.noreply.github.com>
2022-07-11 20:43:24 -0400

    Update Search-GPOs.ps1

--- Search-GPOs/Search-GPOs.ps1
+++ Search-GPOs/Search-GPOs.ps1
@@ -1,122 +1,91 @@
+function Search-GPOs {
 
-<#PSScriptInfo
+    <#
 
-.VERSION 1.0
+    .Synopsis
+    Search Gorup Policies for a given string.
 
-.GUID 12689a1c-0efb-4a96-831f-35aafbb0917a
+    .Description
+    Search Gorup Policies for a given string and display results it in a gridview or export it to a CSV.
 
-.AUTHOR hello@JohnJHeisler.com
+    .Parameter String
+    The string to search GPOs for. Can be an array.
 
-.COMPANYNAME John Heisler
+    .Parameter DomainName
+    Set the Domain Name. Default : $env:USERDNSDOMAIN.
 
-.COPYRIGHT 2022 John Heisler. All rights reserved.
+    .Parameter OutCSV
+    Set the CSV path to export Results to.
 
-.TAGS GroupPolicy GPO JohnHeisler
+    .Parameter Gridview
+    Dispaly Results in a Gridview
 
-.LICENSEURI https://github.com/Snowstuff123/Powershell_Scripts/License
+    .Example
+    # Search GPOs for "FolderRedirect" and dispaly Results in a Gridview.
+    Search-GPO -String FolderRedirect -Gridview
 
-.PROJECTURI https://github.com/Snowstuff123/Powershell_Scripts
+    .Example
+    # Search GPOs for "FolderRedirect" and export Results to C:\setup\Results.csv.
+    Search-GPO -String FolderRedirect -OutCSV "C:\setup\Results.csv"
 
-.ICONURI 
+    #> 
 
-.EXTERNALMODULEDEPENDENCIES grouppolicy
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string[]]
+        $String = (Read-Host -Prompt "String to Search"),
 
-.REQUIREDSCRIPTS 
+        [Parameter()]
+        [string]
+        $DomainName = $env:USERDNSDOMAIN,
 
-.EXTERNALSCRIPTDEPENDENCIES 
+        [Parameter()]
+        [ValidateScript({Test-Path $_})]
+        [string]
+        $OutCSV,
 
-.RELEASENOTES
-Search-GPOs supports the following features:
-A list of strings to search
-Choosing the Domain to find GPOs in
-Exporting results to a CSV file
-Displaying results in PS Gridview
+        [Parameter()]
+        [switch]
+        $Gridview
+    )
+    Begin{
+        Import-Module grouppolicy
 
-#>
+        $result = @()
 
-<#
+        Write-Host "Searching for GPOs in $DomainName"
+    }
+    Process{
+        $allGposInDomain = Get-GPO -All -Domain $DomainName
 
-.Synopsis
-Search Gorup Policies for a given string.
+        Write-Host $('Starting search for ' + $String + '....')
 
-.Description
-Search Gorup Policies for a given string and display results it in a gridview or export it to a CSV.
-
-.Parameter String
-The string to search GPOs for. Can be an array.
-
-.Parameter DomainName
-Set the Domain Name. Default : $env:USERDNSDOMAIN.
-
-.Parameter OutCSV
-Set the CSV path to export Results to.
-
-.Parameter Gridview
-Dispaly Results in a Gridview
-
-.Example
-# Search GPOs for "FolderRedirect" and dispaly Results in a Gridview.
-Search-GPO -String FolderRedirect -Gridview
-
-.Example
-# Search GPOs for "FolderRedirect" and export Results to C:\setup\Results.csv.
-Search-GPO -String FolderRedirect -OutCSV "C:\setup\Results.csv"
-
-#> 
-
-[CmdletBinding()]
-param (
-    [Parameter()]
-    [string[]]
-    $String = (Read-Host -Prompt "String to Search"),
-
-    [Parameter()]
-    [string]
-    $DomainName = $env:USERDNSDOMAIN,
-
-    [Parameter()]
-    [ValidateScript({Test-Path $_})]
-    [string]
-    $OutCSV,
-
-    [Parameter()]
-    [switch]
-    $Gridview
-)
-Begin{
-    Import-Module grouppolicy
-    
-    $result = @()
-
-    Write-Host "Searching for GPOs in $DomainName"
-}
-Process{
-    $allGposInDomain = Get-GPO -All -Domain $DomainName
-
-    Write-Host $('Starting search for ' + $String + '....')
-
-    foreach ($gpo in $allGposInDomain) {
-        $report = Get-GPOReport -Guid $gpo.Id -ReportType Xml
-        if ($report -match $String) {
-            Write-Host "---> Match in: $($gpo.DisplayName) <---" -ForegroundColor Magenta
-            $result += [PSCustomObject]@{
-                Name = $gpo.DisplayName
-                GUID = $gpo.id
-                SearchString = $String
+        foreach ($gpo in $allGposInDomain) {
+            $report = Get-GPOReport -Guid $gpo.Id -ReportType Xml
+            if ($report -match $String) {
+                Write-Host "---> Match in: $($gpo.DisplayName) <---" -ForegroundColor Magenta
+                $result += [PSCustomObject]@{
+                    Name = $gpo.DisplayName
+                    GUID = $gpo.id
+                    SearchString = $String
+                }
+            }
+            else {
+                Write-Host "Nothing in: $($gpo.DisplayName)" -ForegroundColor Green
             }
         }
-        else {
-            Write-Host "Nothing in: $($gpo.DisplayName)" -ForegroundColor Green
+    }
+    End{
+        If($Gridview){
+            $result | Out-GridView -ErrorAction Stop
+            Write-Host 'Gridview Opened'
+        }
+        If($OutCSV){
+            $result | Export-CSV -Path $OutCSV -NoTypeInformation -Append -ErrorAction Stop
+            Write-Host 'CSV Exported'
         }
     }
 }
-End{
-    If($Gridview){
-        $result | Out-GridView -ErrorAction Stop
-        Write-Host 'Gridview Opened'
-    }
-    If($OutCSV){
-        $result | Export-CSV -Path $OutCSV -NoTypeInformation -Append -ErrorAction Stop
-        Write-Host 'CSV Exported'
-    }
-}
+
+Search-GPOs
